f7c0c5930b83aa4f5078d8256dd069cb733e5514
Johnny Franks <johnnycfranks@gmail.com>
2025-05-25 00:58:25 -0400

    Initial Commit

---
+++ CardVault/CardVaultApp.swift
@@ -0,0 +1,17 @@
+//
+//  CardVaultApp.swift
+//  CardVault
+//
+//  Created by Johnny Franks on 5/25/25.
+//
+
+import SwiftUI
+
+@main
+struct CardVaultApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
